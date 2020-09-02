45caa0d2deb2f6888366512b139b3c44183b092c
logpoint <basanta83chaulagain@gmail.com>
2020-09-02 22:36:09 +0545

    optimized code

--- Linked List/getIntersectionNode.py
+++ Linked List/getIntersectionNode.py
@@ -6,6 +6,9 @@
 
 class Solution:
     def getIntersectionNode(self, headA, headB):
+        if(headA==None or headB==None):
+            return None
+
         valA=[]
         while(headA!=None):
             valA.append(headA)
@@ -19,3 +22,11 @@ class Solution:
             headB=headB.next
             
         return None
+
+        #optimized code
+        l1 = headA
+        l2 = headB
+        while(l1 != l2):
+            l1 = headB if l1==None else l1.next
+            l2 = headA if l2==None else l2.next
+        return l1
